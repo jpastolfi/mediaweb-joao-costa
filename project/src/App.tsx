f036a9b9dc2781c7d5ad3e049df976045c6a1326
Joao Astolfi <jpastolfi@gmail.com>
2025-03-07 18:29:59 +0000

    Cleanup, svgs from Figma

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import config from '../public/config.svg'
+import heart from '../public/heart.svg'
+import home from '../public/home.svg'
+import logo from '../public/logo.svg'
+import pie_chart from '../public/pie-chart.svg'
+import user from '../public/user.svg'
 
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='bg-black'>
+      <img src={logo} className="logo" alt="Company logo" />
+      <img src={config} className="logo" alt="Configuration icon" />
+      <img src={heart} className="logo" alt="Heart icon" />
+      <img src={home} className="logo" alt="Home icon" />
+      <img src={pie_chart} className="logo" alt="Pie chart icon" />
+      <img src={user} className="logo" alt="User icon" />
+    </div>
   )
 }
 
