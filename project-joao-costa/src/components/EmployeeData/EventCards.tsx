a2d5b2e924d0a4487103fefcd7dadbbad920c3cc
Joao Astolfi <jpastolfi@gmail.com>
2025-03-10 14:19:36 +0000

    fix: image overlay, svg missing a piece

--- project-joao-costa/src/components/EmployeeData/EventCards.tsx
+++ project-joao-costa/src/components/EmployeeData/EventCards.tsx
@@ -7,11 +7,11 @@ import { Separator } from "../ui/separator";
 export function EventCard({ title, time, coach, participants, active, activity }: { title: string, time: string, coach: string, participants: string[], active: boolean, activity: string }) {
   return (
     <div className={`${active ? "bg-card-dark-blue text-white" : "bg-light-blue text-card-text-dark-blue"} p-4 rounded-lg relative flex flex-col items-start w-10/12`}>
-      <h4 className="text-sm font-semibold py-1">{title}</h4>
-      <p className="text-sm pb-2">{time}</p>
-      <p className="text-sm pb-2">Coach: {coach}</p>
+      <h4 className="text-sm font-semibold py-1 z-999">{title}</h4>
+      <p className="text-sm pb-2 z-999">{time}</p>
+      <p className="text-sm pb-2 z-999">{(activity === 'Health' ? '' : 'Coach: ') + coach}</p>
       {(activity === 'Sport' || activity === 'Health') && (
-        <Separator className={`${active ? "bg-white" : "bg-card-separator my-2"}`} />
+        <Separator className={`${active ? "bg-white" : "bg-card-separator my-2"} z-999`} />
       )}
       {activity === 'Sport' && (
         <div className="flex items-center mt-4">
@@ -34,11 +34,11 @@ export function EventCard({ title, time, coach, participants, active, activity }
       )}
     <div>
       <div className={`absolute ${active ? 'top-0 h-4/5 right-5' : 'bottom-0 h-2.5/5 right-0'} w-2/5`}>
+        {activity === 'Sport' && (
+          <img src={medal} alt="medal" className="w-full h-full object-cover opacity-80" />
+        )}
         {activity === 'Health' && (
           <img src={caduceus} alt="caduceus" className="w-full h-full object-cover" />
-        )}
-        {activity === 'Sport' && (
-          <img src={medal} alt="medal" className="w-full h-full object-cover" />
         )}
         {activity === 'Meditation' && (
           <img src={lotus} alt="lotus" className="w-full h-full object-cover" />
