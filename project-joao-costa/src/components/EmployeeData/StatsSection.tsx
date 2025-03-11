528d8d1df42200fdd1d4f87d1554b2a9057e6edb
Joao Astolfi <jpastolfi@gmail.com>
2025-03-11 11:14:42 +0000

    refactor: CSS simplification

--- project-joao-costa/src/components/EmployeeData/StatsSection.tsx
+++ project-joao-costa/src/components/EmployeeData/StatsSection.tsx
@@ -4,18 +4,18 @@ export function StatsSection() {
   return (
     <div className="flex justify-around bg-light-blue py-2 px-4 rounded-2xl contain-content w-10/12">
       <div className="text-center">
-        <p className="text-sm text-text-light-blue">Blood</p>
-        <p className="text-lg text-text-blue font-semibold">O+</p>
+        <p className="stats-title">Blood</p>
+        <p className="stats-data">O+</p>
       </div>
       <Separator orientation="vertical" className="bg-text-light-blue" />
       <div className="text-center">
-        <p className="text-sm text-text-light-blue">Height</p>
-        <p className="text-lg text-text-blue font-semibold">186cm</p>
+        <p className="stats-title">Height</p>
+        <p className="stats-data">186cm</p>
       </div>
       <Separator orientation="vertical" className="bg-text-light-blue" />
       <div className="text-center">
-        <p className="text-sm text-text-light-blue">Weight</p>
-        <p className="text-lg text-text-blue font-semibold">90kg</p>
+        <p className="stats-title">Weight</p>
+        <p className="stats-data">90kg</p>
       </div>
     </div>
   );
--- project-joao-costa/src/components/Sidebar.tsx
+++ project-joao-costa/src/components/Sidebar.tsx
@@ -11,16 +11,16 @@ export default function Sidebar() {
     <div className='flex flex-col items-center justify-between h-screen w-1/12 bg-main-purple rounded-r-3xl py-6 absolute left-0 z-1'>
       <div className='flex items-center justify-center gap-2'>
         <img src={logo} className="w-9 h-7" alt="Company logo" />
-        <img src={logo_arrow} className="w-6 h-6" alt="Expand arrow" />
+        <img src={logo_arrow} className="size-6" alt="Expand arrow" />
       </div>
       <div className='flex flex-col items-center justify-center gap-6'>
-        <img src={heart} className="w-6 h-6" alt="Heart icon" />
-        <img src={home} className="w-6 h-6" alt="Home icon" />
-        <img src={pie_chart} className="w-6 h-6" alt="Pie chart icon" />
-        <img src={user} className="w-12 h-12" alt="User icon" />
+        <img src={heart} className="size-6" alt="Heart icon" />
+        <img src={home} className="size-6" alt="Home icon" />
+        <img src={pie_chart} className="size-6" alt="Pie chart icon" />
+        <img src={user} className="size-12" alt="User icon" />
       </div>
       <div>
-        <img src={config} className="w-6 h-6" alt="Configuration icon" />
+        <img src={config} className="size-6" alt="Configuration icon" />
       </div>
     </div>
   )
